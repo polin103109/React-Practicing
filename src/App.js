1c1272445d4179326f0a08e318e36ac6fd24927d
polin103109 <sabrinpolinece18@gmail.com>
2023-11-29 04:53:42 +0600

    making a small mini calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import "./App.css";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 
 function App() {
   const [number1, setnumber1] = useState(0);
   const [number2, setnumber2] = useState(0);
   const [sum, setSum] = useState(); //using usestate hook
-  const refelement = useRef(""); //for dom manipulation
-  useEffect(() => {
-    console.log("component mounted");
-  }, [number1]); //using useEffect
 
   function handleAdd() {
     setSum(number1 + number2);
-
-    refelement.current.style.color = "red"; // Accessing DOM styles using useRef
+  }
+  function handleSubstract() {
+    setSum(number1 - number2);
   }
   return (
     <div className="App">
       <h1>Calculate</h1>
       <input
-        ref={refelement}
         type="number"
         value={number1}
         onChange={(e) => setnumber1(parseInt(e.target.value))} // convert string to int
@@ -30,7 +26,10 @@ function App() {
         value={number2}
         onChange={(e) => setnumber2(parseInt(e.target.value))}
       ></input>
+      <br />
       <button onClick={handleAdd}>ADD</button>
+
+      <button onClick={handleSubstract}>Substract</button>
       <br />
       <span>Result is:{sum}</span>
     </div>
