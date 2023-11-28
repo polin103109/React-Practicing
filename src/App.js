0dc47468a04171d987e9b3658a64daed676ce2e8
polin103109 <sabrinpolinece18@gmail.com>
2023-11-29 04:05:52 +0600

    started using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0); //using usestate hook
+  useEffect(() => {
+    console.log("component mounted");
+  }, [count]); //using useEffect
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>REACTTTT</h1>
+
+      <button onClick={handleClick}>Click here</button>
+      <br />
+      <span>{count}</span>
     </div>
   );
 }
